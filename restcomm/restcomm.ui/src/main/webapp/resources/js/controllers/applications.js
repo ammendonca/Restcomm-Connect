2443d10e9a9a93973900277e8fb3cb33009d534d
Orestis Tsakiridis <otsakir@gmail.com>
2017-10-27 15:48:47 +0300

    Client-side filtering for application listing view and 'Applications' navbar item
    Refers Restcomm/Restcomm-Connect#2561

--- restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/applications.js
+++ restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/applications.js
@@ -1,6 +1,4 @@
 angular.module('rcApp.controllers').controller('ApplicationsCtrl', function ($scope, RCommApplications, SessionService) {
-    console.log('IN ApplicationsCtrl');
-
     var accountSid = SessionService.get("sid");
     $scope.appsList = RCommApplications.query({accountSid: accountSid, includeNumbers: true});
 });
